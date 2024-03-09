c5627a54a9da315dcc6eebd94012b1967ac92507
tmdragon2 <133560385+tmdragon2@users.noreply.github.com>
2024-03-09 00:00:13 -0500

    Update 3
    I added a bunch of terrible checks for death n stuff ill add coyote jump next update

---
+++ Get Gamejam/Platform.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+@onready var audio_player = $Area2D/AudioStreamPlayer2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	audio_player.play()
+	print("audio played")
--- Get Gamejam/node_2d.gd
+++ Get Gamejam/node_2d.gd
@@ -8,6 +8,14 @@ const JUMP_VELOCITY = -600.0
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var start_pos = global_position
 
+
+func _process(delta):
+	if self.position.y >= 750.0:
+		global_position = start_pos
+		var start_pos = true
+		print("player died")
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -31,3 +39,4 @@ func _physics_process(delta: float) -> void:
 func _on_hurtbox_detector_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemy"):
 		global_position = start_pos
+		
